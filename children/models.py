b9071a5d4ffb1d667ce1b0bb837c45449a4e8738
Ubuntu <ubuntu@ip-172-31-35-122.ec2.internal>
2019-10-25 12:58:04 +0000

    Children app added

---
+++ children/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Children(models.Model):
+    
+    GENDER = (
+        ('M','Male'),
+        ('F','Female')
+    )
+    name = models.CharField(max_length=254, default="")
+    gender = models.CharField(max_length=6, choices=GENDER)
+    birthday =models.DateField()
+    enjoys = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
