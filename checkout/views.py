2d5a386f26860d7a7f3ee927e820d7f21469b650
Ubuntu <ubuntu@ip-172-31-35-122.ec2.internal>
2019-10-30 11:23:14 +0000

    Checkout debugged

--- checkout/views.py
+++ checkout/views.py
@@ -8,61 +8,54 @@ from django.utils import timezone
 from children.models import Child
 import stripe
 
-
 # Create your views here.
-
 stripe.api_key = settings.STRIPE_SECRET
+
 
 @login_required()
 def checkout(request):
-    if request.method=="POST":
-        print("method POST")
+    if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-        
+
         if order_form.is_valid() and payment_form.is_valid():
-            print("order form valid")
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
-            
+
             cart = request.session.get('cart', {})
             total = 0
             for id, donation in cart.items():
                 child = get_object_or_404(Child, pk=id)
-                total += donation
+                total += donation 
                 order_line_item = OrderLineItem(
-                    order = order, 
-                    child = child, 
-                    donation = donation
-                    )
+                    order=order,
+                    child=child,
+                    donation=donation
+                )
                 order_line_item.save()
-                
+            
             try:
                 customer = stripe.Charge.create(
-                    amount = int(total * 100),
-                    currency = "EUR",
-                    description = request.user.email,
-                    card = payment_form.cleaned_data['stripe_id'],
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-                
+            
             if customer.paid:
-                print("customer paid")
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
-                return redirect(reverse('products'))
+                return redirect(reverse('children'))
             else:
-                print("customer nor paid")
                 messages.error(request, "Unable to take payment")
         else:
-            print("error")
             print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card!")
     else:
-        print("order not good")
         payment_form = MakePaymentForm()
         order_form = OrderForm()
-        
-    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
