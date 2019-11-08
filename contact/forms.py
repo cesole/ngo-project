812e1816c61c8d1e4c579399b3cceb661adb297d
Ubuntu <ubuntu@ip-172-31-35-122.ec2.internal>
2019-11-08 10:18:40 +0000

    Contact form updated, CSS updated, footer updated

--- contact/forms.py
+++ contact/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
+    contact_subject = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
     content = forms.CharField(widget=forms.Textarea, required=True)
--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.core.mail import EmailMessage
 from .forms import *
+from django.template import loader
 
 
 def Contact(request):
@@ -11,28 +12,19 @@ def Contact(request):
         form = Contact_Form(data=request.POST)
 
         if form.is_valid():
-            contact_name = request.POST.get('contact_name')
-            contact_email = request.POST.get('contact_email')
-            contact_content = request.POST.get('content')
-
-            template = get_template('contact_form.txt')
-            context = {
-                'contact_name' : contact_name,
-                'contact_email' : contact_email,
-                'contact_content' : contact_content,
-            }
-            
-            content = template.render(context)
-
-            email = EmailMessage(
-                "New contact form email",
-                content,
-                "Creative web" + '',
-                ['myfriendkhendelwal@gmail.com'],
-                headers = { 'Reply To': contact_email }
-            )
-
-            email.send()
-
-            return redirect('index')
+            name = request.POST.get('name')
+            subject = request.POST.get('subject')
+            email = request.POST.get('email')
+            content = request.POST.get('content')
+            send_mail(subject, content, 'ceci.abt@gmail.com', [email], fail_silently=False)
+            if send_mail:
+                messages.success(request, "Your message has been sent")
+                return render(request, "contact")
+            else:
+                messages.error(request,'Error in form.Try Again')
+                return redirect(request, "contact")
+        else:
+            form = EmailForm(request.POST)
     return render(request, 'contact.html', {'form':Contact_Form })
+    
+    
